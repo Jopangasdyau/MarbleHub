dbe8b8a949aaa61c228fd0b0d3763f44e515461b
test <lerlerlor236@gmail.com>
2025-10-31 17:03:29 +0700

    Update bloxpvp.lua
    fix unbreakable bug

--- bloxpvp.lua
+++ bloxpvp.lua
@@ -1,7 +1,7 @@
 local Library = loadstring(game:HttpGetAsync("https://github.com/ActualMasterOogway/Fluent-Renewed/releases/latest/download/Fluent.luau"))()
 
 local Window = Library:CreateWindow{
-    Title = 'bfpvp',
+    Title = `blox fruit`,
     SubTitle = "",
     TabWidth = 160,
     Size = UDim2.fromOffset(830, 525),
@@ -19,22 +19,58 @@ local Tabs = {
     }
 }
 
+local p = game:GetService("Players")
+local l = p.LocalPlayer
+local c = workspace:WaitForChild("Characters")
+
+local function f(s)
+	for _, model in pairs(c:GetChildren()) do
+		if model:FindFirstChild("HumanoidRootPart") and model.Name ~= l.Name then
+			model.HumanoidRootPart.Size = Vector3.new(s, s, s)
+		end
+	end
+end
+
+local Slider = Tabs.Main:CreateSlider("Slider", {
+	Title = "Hitbox",
+	Description = "",
+	Default = 1,
+	Min = 1,
+	Max = 200,
+	Rounding = 1,
+	Callback = function(x)
+		v = x
+		f(x)
+	end
+})
+
+p.PlayerAdded:Connect(function(player)
+	p.CharacterAdded:Connect(function(char)
+		repeat task.wait() until char:FindFirstChild("HumanoidRootPart")
+		if char.Name ~= l.Name then
+			char.HumanoidRootPart.Size = Vector3.new(v, v, v)
+		end
+	end)
+end)
+
+f(x)
+
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
-local Slider = Tabs.Main:CreateSlider("Slider", {
-    Title = "Hitbox",
-    Description = "Resize all player hitboxes except you",
-    Default = 1,
-    Min = 1,
-    Max = 100,
-    Rounding = 1,
-    Callback = function(Value)
-        for _, model in pairs(workspace.Characters:GetChildren()) do
-            if model:FindFirstChild("HumanoidRootPart") and model.Name ~= LocalPlayer.Name then
-                model.HumanoidRootPart.Size = Vector3.new(Value, Value, Value)
-            end
-        end
-        print("Hitbox size changed to:", Value)
+local Toggle2 = Tabs.Main:CreateToggle("MyToggle", {Title = "Unbreakable", Default = false})
+
+Toggle2:OnChanged(function()
+    if not Toggle2Interacted then
+        Toggle2Interacted = true
+        return
     end
-})
+
+    BuyEnabled = not BuyEnabled
+
+    if BuyEnabled then
+        workspace.Characters:FindFirstChild(LocalPlayer.Name):SetAttribute("UnbreakableAll", true)
+	else
+		workspace.Characters:FindFirstChild(LocalPlayer.Name):SetAttribute("UnbreakableAll", false)
+	end
+end)
